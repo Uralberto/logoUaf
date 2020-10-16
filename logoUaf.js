febbe36b468f4b788fa217bd7b23199d26fd3b75
Uriel Arevalo <urievalo@hotmail.com>
2020-10-16 15:26:48 -0500

    Primer commit proyecto logoUaf

---
+++ logoUaf.js
@@ -0,0 +1,11 @@
+const letraA = document.getElementById("letra-A");
+const brazoLetraU = document.getElementById("brazo-letra-U");
+
+const anilloExterno = document.getElementById("anilloExterno");
+
+const dashArrayAnilloExterno = anilloExterno.getTotalLength();
+
+console.log(dashArrayAnilloExterno);
+
+const dashArrayLetraA = letraA.getTotalLength();
+console.log(dashArrayLetraA);
